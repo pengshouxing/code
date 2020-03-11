ae2c9ce88ce2766368fbc5991393f63b8eb5028a
xiaopeng <xiaopeng@LAPTOP-TNTQ9Q2M>
2020-03-12 01:11:23 +0800

    关闭了自动ack机制

--- src/main/java/com/test/mq/rabbitmq/MqReceive.java
+++ src/main/java/com/test/mq/rabbitmq/MqReceive.java
@@ -14,13 +14,14 @@ public class MqReceive {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         //durable==false代表队列持久
-        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'"+consumerTag);
         };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        //关闭了自动ack机制,队列如果没有接受到消费者ack,则队列消息等待消费者结束又会变成准备状态，消息者又可以重新取出
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
 }
