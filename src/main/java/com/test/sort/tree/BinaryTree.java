c5f6b900c91d74cf9561d73f4022b7af0d5f9caa
xiaopeng <xiaopeng@LAPTOP-RO55QKG9>
2020-05-07 15:21:09 +0800

    二叉树的四种遍历算法

--- src/main/java/com/test/sort/tree/BinaryTree.java
+++ src/main/java/com/test/sort/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.test.sort.tree;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -8,12 +10,15 @@ import java.util.concurrent.LinkedBlockingQueue;
  * 1.前序遍历:先访问根节点再访问左子树最后再访问右子树。
  * 2.中序遍历:先访问左子树再访问根节点最后再访问右子树。
  * 3.后序遍历:先访问左子树再访问右子树最后再访问根节点。
- * 4.层序遍历:层序遍历:从上到下分别取出节点。
+ * 4.层序遍历:层序遍历迭代方式:从上到下分别取出节点。
+ *          层序遍历递归方式:同一层存放在同一个List中。
  * 
  * @author xiaopeng
  *
  */
 public class BinaryTree {
+	
+	private static List<List<Integer>> list = new ArrayList<>();
 
 	public static void main(String[] args) {
 
@@ -32,17 +37,24 @@ public class BinaryTree {
 		tree.rightNode.rightNode.leftNode = new Tree(21);
 		tree.rightNode.rightNode.rightNode = new Tree(24);
 
+		BinaryTree binaryTree = new BinaryTree();
 		System.out.print("前序遍历:");
-		new BinaryTree().preorderTraversal(tree);
+		binaryTree.preorderTraversal(tree);
 		System.out.println();		
         System.out.print("中序遍历:");		
-		new BinaryTree().inorderTraversal(tree);
+        binaryTree.inorderTraversal(tree);
 		System.out.println();	
 		System.out.print("后序遍历:");
-		new BinaryTree().postorderTraversal(tree);
+		binaryTree.postorderTraversal(tree);
 		System.out.println();	
 		System.out.print("层序遍历:");
-		new BinaryTree().sequenceorderTraversal(tree);
+		binaryTree.sequenceorderTraversal(tree);
+		System.out.println();	
+		System.out.print("层序遍历递归方式:");
+		binaryTree.sequenceorderTraversalRecursionWay(tree, 0);
+		for (List<Integer> newList : list) {
+			System.out.print(newList);
+		}
 	}
 
 	// 前序遍历:先访问根节点再访问左子树,最后再访问右子树。
@@ -69,7 +81,7 @@ public class BinaryTree {
 		System.out.print(tree.val+" ");
 	}
 	
-	// 层序遍历:从上到下分别取出节点。分别放入队列中，再从队列中取出。
+	// 层序遍历迭代方式:从上到下分别取出节点。分别放入队列中，再从队列中取出。
 	public void sequenceorderTraversal(Tree tree) {
 		Queue<Tree> queue = new LinkedBlockingQueue<Tree>();
 		queue.offer(tree);
@@ -83,6 +95,18 @@ public class BinaryTree {
 				queue.offer(tree.rightNode);
 			}
 		}
+	}
+	
+	// 层序遍历递归方式:同一层存放在同一个List中。不同层则新建List存放节点,从左到右添加
+	public void sequenceorderTraversalRecursionWay(Tree tree,int level) {
+		if(tree == null) return ;
+		//判断当前层是否新建过list，没有则新建
+		if(level == list.size()) {
+			list.add(new ArrayList<Integer>());
+		}
+		list.get(level).add(tree.val);
+		sequenceorderTraversalRecursionWay(tree.leftNode, level+1);
+		sequenceorderTraversalRecursionWay(tree.rightNode, level+1);
 	}
 
 }
