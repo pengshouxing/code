fe0241d9f90b25a6fffac5eb8f6b4b6d9b3bd545
xiaopeng <xiaopeng@LAPTOP-RO55QKG9>
2020-10-01 18:29:30 +0800

    链表尾插法

---
+++ src/main/java/com/test/sort/linkedlist/LinkedListCopy.java
@@ -0,0 +1,38 @@
+package com.test.sort.linkedlist;
+
+/**
+ * 链表尾插法参考HashMap
+ * @author xiaopeng
+ *
+ */
+public class LinkedListCopy {
+
+	public static void main(String[] args) {
+		SingleNode node = new SingleNode(3);
+		node.next = new SingleNode(6);
+		node.next.next = new SingleNode(3);
+		node.next.next.next = new SingleNode(4);
+
+		SingleNode singleNode = new LinkedListCopy().copyLinkedList(node);
+		singleNode.toString(singleNode);
+
+	}
+
+	public SingleNode copyLinkedList(SingleNode from) {
+		SingleNode firstNode = null;
+		SingleNode lastNode = null;
+		while (from != null) {
+			if (from.val % 2 == 0) {
+				if (firstNode == null) {
+					firstNode = from;
+				} else {
+					lastNode.next = from;
+				}
+				lastNode = from;
+			}
+			from = from.next;
+		}
+		return firstNode;
+	}
+
+}
