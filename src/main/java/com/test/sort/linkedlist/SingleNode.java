3549979cbbba95d4f69e0d184b96444fd0f8550a
xiaopeng <xiaopeng@LAPTOP-RO55QKG9>
2020-11-11 23:04:59 +0800

    单链表添加元素还需要优化

--- src/main/java/com/test/sort/linkedlist/SingleNode.java
+++ src/main/java/com/test/sort/linkedlist/SingleNode.java
@@ -1,8 +1,7 @@
 package com.test.sort.linkedlist;
 
-
 public class SingleNode {
-	
+
 	int val;
 	SingleNode next;
 
@@ -12,15 +11,35 @@ public class SingleNode {
 
 	public void toString(SingleNode node) {
 		while (node != null) {
-			System.out.print(node.val+">");
+			System.out.print(node.val + ">");
 			node = node.next;
 		}
 	}
-	
+
+	/**
+	 * 在尾部添加
+	 * 
+	 * @param singleNode
+	 * @param addNode
+	 */
+	public void addNodeInTail(SingleNode singleNode, SingleNode addNode) {
+		if (singleNode == null) {
+			addNode = singleNode;
+			System.out.println(addNode);
+		} else {
+			while (singleNode.next != null) {
+				singleNode = singleNode.next;
+			}
+			singleNode.next = addNode;
+		}
+	}
+
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("abc");
-		sb.append("def");
+		SingleNode node = null;
+		SingleNode addNode = new SingleNode(2);
+		addNode.addNodeInTail(node, addNode);
+		System.out.println(node);
+		addNode.toString(node);
 	}
 
 }
